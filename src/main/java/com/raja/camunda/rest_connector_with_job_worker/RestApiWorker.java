26ac23f707d0460bcb853fb8532d1c778324911b
Rajaguru <rajagurup11@gmail.com>
2024-10-22 22:09:29 +0530

    first commit

---
+++ src/main/java/com/raja/camunda/rest_connector_with_job_worker/RestApiWorker.java
@@ -0,0 +1,28 @@
+package com.raja.camunda.rest_connector_with_job_worker;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RestApiWorker {
+
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  @JobWorker(type = "io.camunda:http-json:1")
+  public void handleRestApiCall(final JobClient client, final ActivatedJob job) {
+    var url = job.getVariablesAsMap().get("url");
+    var method = job.getVariablesAsMap().get("method");
+
+    // Example REST API Call
+    var result = restTemplate.getForObject("http://localhost:8901/hello", String.class);
+
+    // Log the response or process it
+    System.out.println("API response: " + result);
+
+    // Complete the job
+    client.newCompleteCommand(job.getKey()).send().join();
+  }
+}
---
+++ src/main/java/com/raja/camunda/rest_connector_with_job_worker/RestConnectorWithJobWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.raja.camunda.rest_connector_with_job_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConnectorWithJobWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConnectorWithJobWorkerApplication.class, args);
+	}
+
+}
